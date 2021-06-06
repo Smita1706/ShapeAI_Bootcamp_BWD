dd91d92e4559f823e363ff1ec85c55cce50f66f8
Smita1706 <85427705+Smita1706@users.noreply.github.com>
2021-06-06 13:14:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import List from "./List";
+import Image from "./Image";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <Image />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
